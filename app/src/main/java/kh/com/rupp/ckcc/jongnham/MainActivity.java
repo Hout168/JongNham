9c638ed2ef4d7fe4e071ad048e3240c83d0c73e3
Hout168 <jhout1998@gmail.com>
2018-11-14 07:58:06 +0700

    Done

--- app/src/main/java/kh/com/rupp/ckcc/jongnham/MainActivity.java
+++ app/src/main/java/kh/com/rupp/ckcc/jongnham/MainActivity.java
@@ -1,5 +1,7 @@
 package kh.com.rupp.ckcc.jongnham;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
@@ -8,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
+    Button call,map;
+
     int[] tabIcons = {
                 R.drawable.store, R.drawable.food, R.drawable.event, R.drawable.comment
     };
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout=findViewById(R.id.tabLayout);
         viewPager=findViewById(R.id.viewPager);
 
+        //set up toolbar
         setSupportActionBar(toolbar);
         ActionBar actionBar =getSupportActionBar();
         setTitle("");
@@ -41,6 +48,31 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0;i<tabLayout.getTabCount();i++){
             tabLayout.getTabAt(i).setIcon(tabIcons[i]);
         }
+
+        // Click on Call Now
+        call = findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:096 662 2227"));
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // Click on Map
+        final Uri gmmIntentUri = Uri.parse("google.streetview:cbll=11.5399083,104.9102096");
+        map = findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
